8d3963db57d180844afdd3ccebb626c2a24d0cd6
Nitish Chauhan <nitishc668@gmail.com>
2026-08-19 01:12:18 +0530

    Time: 19 ms (25.15%) | Memory: 49.1 MB (31.06%) - LeetSync

--- 1427-all-elements-in-two-binary-search-trees/all-elements-in-two-binary-search-trees.java
+++ 1427-all-elements-in-two-binary-search-trees/all-elements-in-two-binary-search-trees.java
@@ -17,7 +17,7 @@ class Solution {
     public List<Integer> getAllElements(TreeNode root1, TreeNode root2) {
        List<Integer> ans=new ArrayList<>();
        preOrder1(root1,ans);
-       preOrder2(root2,ans); 
+       preOrder1(root2,ans); 
        Collections.sort(ans);
        return ans;
     }
@@ -28,13 +28,5 @@ class Solution {
         ans.add(root1.val);
         preOrder1(root1.left,ans);
         preOrder1(root1.right,ans);
-    }
-    public void preOrder2(TreeNode root2,List<Integer> ans){
-        if(root2==null){
-            return;
-        }
-        ans.add(root2.val);
-        preOrder2(root2.left,ans);
-        preOrder2(root2.right,ans);
     }
 }
